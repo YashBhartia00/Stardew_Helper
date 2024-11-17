4d287f5d567969d8654d131e6bca5bf7ba6ad117
EstelleGallon <estellegallon@gmail.com>
2024-11-17 15:22:59 +0100

    feat: create grouped bar chart for fishes' prices

--- js/main.js
+++ js/main.js
@@ -99,3 +99,106 @@ function myFilter() {
         }
     }
 }
+
+
+function priceBarChart(selector, fishName){
+    var margin = {top: 10, right: 30, bottom: 20, left: 50},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+    // append the svg object to the body of the page
+    let svg = d3.select(selector)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+    
+    let subgroups = ["Base", "Silver", "Gold", "Irridium"];
+    let groups = ["BP", "FP", "AP"];
+
+    let data = [];
+    subgroups.forEach(subgroup => {
+        let baseRow = ctx.fish_price_breakdown.find(row => row.Name === "BP " + subgroup);
+        let fisherRow = ctx.fish_price_breakdown.find(row => row.Name === "FP " + subgroup);
+        let anglerRow = ctx.fish_price_breakdown.find(row => row.Name === "AP " + subgroup);
+        data.push ({
+            quality: subgroup,
+            "BP": parseInt(baseRow[fishName]),
+            "FP": parseInt(fisherRow[fishName]),
+            "AP": parseInt(anglerRow[fishName])
+        });
+    });
+    console.log(data);
+
+    // fx encodes the fish quality
+    const fx = d3.scaleBand()
+    .domain(subgroups)
+    .range([0, width])
+    .padding(0.2);
+    
+
+    // Add x encodes the profession
+    const x = d3.scaleBand()
+    .domain(groups)
+    .range([0, fx.bandwidth()])
+    .padding(0.05);
+
+    const color = d3.scaleOrdinal()
+    .domain(groups)
+    .range(d3.schemeSpectral[groups.length]);
+
+    // y encodes the price (height of the bars)
+    let y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => Math.max(d["BP"], d["FP"], d["AP"]))]).nice()
+    .range([height, 0]);
+
+    // Show the bars
+    svg.append("g")
+        .selectAll("g")
+        .data(data)
+        .enter()
+        .append("g")
+            .attr("transform", d => "translate(" + fx(d.quality) + ",0)")
+        .selectAll("rect")
+        .data(d => groups.map(key => ({key: key, value: d[key]})))
+        .enter()
+        .append("rect")
+            .attr("x", d => x(d.key))
+            .attr("y", d => y(d.value))
+            .attr("width", x.bandwidth())
+            .attr("height", d => height - y(d.value))
+            .style("fill", d => color(d.key))
+            .append("title")
+                .text(d => `${d.value}g`);
+    
+    // Add y axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+    
+    // Add x axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(fx).tickSize(0));
+
+    // Add legend
+    const legend = svg.append("g")
+        .attr("transform", `translate(10, 0)`);
+
+    const professions = ["Base Profession", "Fisher Profession", "Angler Profession"];
+    professions.forEach((profession, i) => {
+        const legendRow = legend.append("g")
+            .attr("transform", `translate(0, ${i * 20})`);
+        
+        legendRow.append("rect")
+            .attr("width", 10)
+            .attr("height", 10)
+            .attr("fill", color(groups[i]));
+
+        legendRow.append("text")
+            .attr("x", 20)
+            .attr("y", 10)
+            .attr("text-anchor", "start")
+            .text(profession);
+    });
+}
